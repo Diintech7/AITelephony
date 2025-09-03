437ffe8855caedcf065484c3fc97357a43b5e3aa
diinteach7 <diintechteam7@gmail.com>
2025-09-03 17:55:09 +0530

    lets see

--- websocket/aitota.js
+++ websocket/aitota.js
@@ -1629,6 +1629,8 @@ const setupUnifiedVoiceServer = (wss) => {
         deepgramUrl.searchParams.append("language", deepgramLanguage)
         deepgramUrl.searchParams.append("interim_results", "true")
         deepgramUrl.searchParams.append("smart_format", "true")
+        deepgramUrl.searchParams.append("endpointing", "200")
+        deepgramUrl.searchParams.append("utterance_end_ms", "1000")
 
         deepgramWs = new WebSocket(deepgramUrl.toString(), {
           headers: { Authorization: `Token ${API_KEYS.deepgram}` },
